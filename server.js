fed37be779775484353e1cf9b55fc1cea4e2f246
dthrasher90 <dthrasher90@GMAIL.COM>
2017-05-12 09:27:13 -0800

    server updates
    server updates

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res){
   console.log("hello world");
+    res.sendFile('index.html');
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT  ||  5000, function(){
   console.log("Listening on port 3000");
 });
